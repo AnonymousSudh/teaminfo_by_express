9fd7dadd07d678b044125860cea1d44678c3e8fe
Sudhanshu Kumar <64887608+AnonymousSudh@users.noreply.github.com>
2021-08-25 19:19:59 +0530

    webiste on heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = process.env.port || 8000;
 
 
 // built in middleware
@@ -27,7 +28,7 @@ app.get("*", (req, res) => {
     res.render("404");
 });
 
-app.listen(8000,() =>{
+app.listen(port,() =>{
     console.log("listing to port no. 8000");
 })
 
