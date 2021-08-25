3f5c905faa4e231e156ef9c75034429d46e718ba
Sudhanshu Kumar <64887608+AnonymousSudh@users.noreply.github.com>
2021-08-25 14:27:31 +0530

    first commi

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+
+// built in middleware
+const staticpath = path.join(__dirname, "../public");
+app.use(express.static(staticpath));
+
+
+// to set the hbs engine
+app.set('view engine', 'hbs');
+ 
+app.get("/", (req, res) =>{
+    res.render("index");
+});
+
+app.get("/about" ,(req,res)=>{
+    res.render("about");
+    
+});
+app.get("/about/*" ,(req,res)=>{
+    res.render("404");
+    
+});
+app.get("*", (req, res) => {
+    res.render("404");
+});
+
+app.listen(8000,() =>{
+    console.log("listing to port no. 8000");
+})
+
